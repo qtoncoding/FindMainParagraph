e894c3f019b214fcb502100ee9f5219011eff3f9
Thai Quoc Nguyen <thai_ng@outlook.com>
2017-04-06 20:28:45 -0700

    Open image page

--- FindMainParagraph-Client/SummarizePage.xaml.cs
+++ FindMainParagraph-Client/SummarizePage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
 using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -13,6 +14,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
+using MainParagraphWinRT;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -40,8 +45,36 @@ namespace FindMainParagraph_Client
             var file = await picker.PickSingleFileAsync();
             if (file != null)
             {
-                // Do the thing
+                SoftwareBitmap originalBitmap;
+                using (var stream = await file.OpenReadAsync())
+                {
+                    var decoder = await BitmapDecoder.CreateAsync(stream);
+                    originalBitmap = await decoder.GetSoftwareBitmapAsync();
+                }
+
+                if (ParagraphSwitch.IsOn)
+                {
+                    var findMainParagraph = new FindMainParagraph(originalBitmap);
+                    var finalBitmap = findMainParagraph.FinalImage();
+
+                    var finalSource = await GetBitmapSourceAsync(finalBitmap);
+                    Original.Source = finalSource;
+                }
+                else
+                {
+                    var originalSource = await GetBitmapSourceAsync(originalBitmap);
+                    Original.Source = originalSource;
+                }
+                BrowseButton.Visibility = Visibility.Collapsed;
             }
+        }
+
+        private async Task<SoftwareBitmapSource> GetBitmapSourceAsync(SoftwareBitmap bitmap)
+        {
+            var imageSource = new SoftwareBitmapSource();
+            var newbitmap = SoftwareBitmap.Convert(bitmap, BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+            await imageSource.SetBitmapAsync(newbitmap);
+            return imageSource;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
